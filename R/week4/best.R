20beedecf904bdf9e64d3aebc40ff1beef78d290
Ridzuan Mohamad <ridzuanmo@gmail.com>
2016-10-04 22:36:56 +0800

    put enumerate list to check outcome

--- R/week4/best.R
+++ R/week4/best.R
@@ -1,38 +1,45 @@
 best <- function(state, outcome) {
+    ##
     ## define type of accepted outcome
-    type_of_outcome <- c( "heart attack" = "Heart.Attack", "heart failure"="Heart.Failure", "pneumonia"= "Pneumonia")
+    ##
     
+    type_of_outcome <- c( "heart attack" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", 
+                          "heart failure" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", 
+                          "pneumonia"= "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia" )
+    
+    ##
     ## Read outcome data
+    ##
+    
     raw_data <- read.csv(file.path("data","outcome-of-care-measures.csv"), na.string = "Not Available", colClasses = "character")
     
+    ##
     ## Check that state and outcome are valid
-    state_data <- subset(raw_data, State==state, 
-                         select=c(Hospital.Name,
-                                  Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack,
-                                  Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure,
-                                  Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia))
-    
-    names(state_data) <- c("Hospital.Name", "Heart.Attack", "Heart.Failure","Pneumonia")
-    
-    if(nrow(state_data)==0){
-        stop("Invalid State")
-    }
+    ##
     
     if(is.na(type_of_outcome[outcome])){
+        ## Stop if invalid outcome
         stop("invalid outcome")
     }
     
+    ## get subset of data based on State and omit incomplete cases
+    hospital <- na.omit(subset(raw_data, State==state, select=c("Hospital.Name", type_of_outcome[outcome])))
+    
+    names(hospital) <- c("Hospital.Name", "outcome")
+    
+    if(nrow(hospital)==0){
+        ## Stop if state does not exist
+        stop("Invalid State")
+    }
+    
+    ##
     ## Return hospital name in that state with lowest
     ## 30-days death rate
-    
-    ordered_data = NULL
-    
-    ## Selected complete case for each outcome
-    complete_case_data <- state_data[!is.na(state_data[type_of_outcome[outcome]]), ]
+    ##
 
     ## Ordered complete case data
-    ordered_data <- complete_case_data[order(as.numeric(complete_case_data[[type_of_outcome[outcome]]]), 
-                                             complete_case_data["Hospital.Name"]), ]
+    ordered_data <- hospital[order(as.numeric(hospital$outcome), hospital$Hospital.Name), ]
     
+    ## return the top most data
     ordered_data[1, "Hospital.Name"]
 }
--- R/week4/rankhospital.R
+++ R/week4/rankhospital.R
@@ -1,42 +1,64 @@
 rankhospital <- function(state, outcome, num = "best") {
+    ##
     ## define type of accepted outcome
+    ##
+    
     type_of_outcome <- c( "heart attack" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", 
                           "heart failure"="Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", 
                           "pneumonia"= "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia" )
     
+    ##
     ## Read outcome data
+    ##
+    
     raw_data <- read.csv(file.path("data","outcome-of-care-measures.csv"), na.string = "Not Available", colClasses = "character")
     
+    ##
     ## Check that state and outcome are valid
-    hospital <- na.omit(subset(raw_data, State==state, select=c("Hospital.Name", type_of_outcome[outcome])))
-    
-    names(hospital) <- c("Hospital.Name", "Rate")
-    
-    if(nrow(hospital)==0){
-        stop("Invalid State")
-    }
+    ##
     
     if(is.na(type_of_outcome[outcome])){
+        ## Stop if invalid outcome
         stop("invalid outcome")
     }
     
+    ## get subset of data based on State and omit incomplete cases
+    hospital <- na.omit(subset(raw_data, State==state, select=c("Hospital.Name", type_of_outcome[outcome])))
+    
+    ## rename header
+    names(hospital) <- c("Hospital.Name", "Rate")
+    
+    if(nrow(hospital)==0){
+        ## Stop if state does not exist
+        stop("Invalid State")
+    }
+    
+    ##
     ## Return hospital name in that state with the given rank
     ## 30-day death rate
+    ##
     
+    # Sort hospital base on Rate
     ranking <- hospital[order( as.numeric(hospital$Rate), hospital$Hospital.Name), ]
+    
+    # Assign rank to sorted data frame
     ranking$rank <- 1:nrow(ranking)
     
     if(num=="best") {
-        return(ranking[1,"Hospital.Name"])
+        ## Return the best hospital
+        return(ranking[1, "Hospital.Name"])
     }
     
     if(num=="worst") {
-        return(ranking[nrow(ranking),"Hospital.Name"])
+        ## Return the worst hospital
+        return(ranking[nrow(ranking), "Hospital.Name"])
     }
     
     if(num > nrow(ranking)) {
+        ## Return the NA if the rank is not valid
         return(NA)
     }
     
+    ## Return hospital based on rank
     ranking[ranking$rank == num ,"Hospital.Name"]
 }
